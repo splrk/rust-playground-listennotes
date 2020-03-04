05af1058c6d648d1da3c9ccb0bfb289e951dfbd2
Ryan Seal <ryan@code.splrk.net>
2020-03-04 12:28:39 +0200

    Move listen_notes module to separate file

---
+++ src/listen_notes.rs
@@ -0,0 +1,40 @@
+use std::cmp::Ordering;
+use url::Url;
+
+pub struct Podcast {
+    pub id: String,
+    pub rss: Option<Url>,
+    pub email: String,
+    pub image: Option<Url>,
+    pub title: String,
+}
+
+pub struct Episode<'a> {
+    pub id: String,
+    pub link: Option<Url>,
+    pub audio: Option<Url>,
+    pub image: Option<Url>,
+    pub title: String,
+    pub podcast: &'a Podcast,
+    pub thumbnail: Option<Url>,
+    pub description: String,
+    pub pub_date_ms: u128,
+    pub listennotes_url: Option<Url>,
+    pub audio_length_sec: u32,
+    pub explicit_content: bool,
+    pub maybe_audio_invalid: bool,
+    pub listennotes_edit_url: Option<Url>,
+}
+
+impl<'a> Episode<'a> {
+    pub fn length(&self) -> String {
+        let minutes = self.audio_length_sec / 60;
+        let seconds = self.audio_length_sec % 60;
+
+        format!("{:02}:{:02}", minutes, seconds)
+    }
+
+    pub fn cmp(&self, other: &Episode) -> Ordering {
+        self.pub_date_ms.cmp(&other.pub_date_ms)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,19 @@
 use url::Url;
 
-mod listen_notes {
-    use std::cmp::Ordering;
-    use url::Url;
+mod listen_notes;
+use listen_notes::{Podcast, Episode};
 
-    pub struct Podcast {
-        pub id: String,
-        pub rss: Option<Url>,
-        pub email: String,
-        pub image: Option<Url>,
-        pub title: String,
-    }
-
-    pub struct Episode<'a> {
-        pub id: String,
-        pub link: Option<Url>,
-        pub audio: Option<Url>,
-        pub image: Option<Url>,
-        pub title: String,
-        pub podcast: &'a Podcast,
-        pub thumbnail: Option<Url>,
-        pub description: String,
-        pub pub_date_ms: u128,
-        pub listennotes_url: Option<Url>,
-        pub audio_length_sec: u32,
-        pub explicit_content: bool,
-        pub maybe_audio_invalid: bool,
-        pub listennotes_edit_url: Option<Url>,
-    }
-
-    impl<'a> Episode<'a> {
-        pub fn length(&self) -> String {
-            let minutes = self.audio_length_sec / 60;
-            let seconds = self.audio_length_sec % 60;
-
-            format!("{:02}:{:02}", minutes, seconds)
-        }
-
-        pub fn cmp(&self, other: &Episode) -> Ordering {
-            self.pub_date_ms.cmp(&other.pub_date_ms)
-        }
-    }
-}
 
 fn parse_url(url: &str) -> Option<Url> {
     match Url::parse(url) {
-        Err(e) => None,
+        Err(_) => None,
         Ok(parsed_url) => Some(parsed_url),
     }
 }
 
 
 fn main() {
-    let star_wars_7x7 = listen_notes::Podcast {
+    let star_wars_7x7 = Podcast {
         id: String::from("4d3fe717742d4963a85562e9f84d8c79"),
         rss: parse_url("https://www.listennotes.com/c/r/4d3fe717742d4963a85562e9f84d8c79"),
         email: String::from("fx7@sw7x7.com"),
@@ -60,7 +21,7 @@ fn main() {
         title: String::from("Star Wars 7x7 | Star Wars News, Interviews, and More!"),
      };
 
-    let episode775 = listen_notes::Episode {
+    let episode775 = Episode {
         id: String::from("02f0123246c944e289ee2bb90804e41b"),
         link: parse_url("http://sw7x7.libsyn.com/1775-happy-75th-birthday-george-lucas?utm_source=listennotes.com&utm_campaign=Listen+Notes&utm_medium=website"),
         audio: parse_url("https://www.listennotes.com/e/p/02f0123246c944e289ee2bb90804e41b/"),
@@ -77,7 +38,7 @@ fn main() {
         listennotes_edit_url: parse_url("https://www.listennotes.com/e/02f0123246c944e289ee2bb90804e41b/#edit")
     };
 
-    let episode864 = listen_notes::Episode {
+    let episode864 = Episode {
         id: String::from("4e7c59e10e4640b98f2f3cb1777dbb43"),
         link: parse_url("http://sw7x7.libsyn.com/864-part-2-of-my-new-conversation-with-bobby-roberts?utm_source=listennotes.com&utm_campaign=Listen+Notes&utm_medium=website"),
         audio: parse_url("https://www.listennotes.com/e/p/4e7c59e10e4640b98f2f3cb1777dbb43/"),
